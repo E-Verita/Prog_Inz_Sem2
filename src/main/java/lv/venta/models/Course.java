a1c645b90211f168a9b7d2904b8ed895d7db61f2
Everita Vecbērza <everita.vecberza@gmail.com>
2023-05-29 10:32:55 +0300

    New: Course class

---
+++ src/main/java/lv/venta/models/Course.java
@@ -0,0 +1,45 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name="course_table")
+@Entity
+@Getter
+@Setter  //pielāgosies anotācijām
+@NoArgsConstructor
+@ToString
+public class Course {
+	
+	@Setter(value=AccessLevel.NONE)
+	@Column(name="Idc")
+	private long idc;
+	
+	@Column(name="Title")
+	@Pattern(regexp="[A-ZĀČĒĪĶĻŅŠŪŽ]{1}[a-zāčēīķļņšūž\\ ]+", message="Pirmajam burtam jābūt lielajam")
+	@NotNull
+	@Size(min = 3, max = 30)
+	private String title;
+	
+	@Min(value=1)
+	@Max(value=20)
+	@Column(name="creditPoints")
+	private int creditPoints;
+	
+	//TODO: izveidot argumenta konstruktoru
+	//TODO: Izveidot saites
+	
+	
+}
